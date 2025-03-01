afadd656d0755bccff91ba4128655b86d5579774
Max DeSousa <maxdesousa@protonmail.com>
2025-03-01 15:22:21 -0500

    Adding some simple subfuntions to handle button pressing

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
 
 #define MOTION_SENSOR_PIN 0
 #define SERVO_PIN 1
+#define STEADY_STATE_PWM 0
+#define BUTTON_PRESS_PWM 255
 
 bool MotionDetected = false;
+uint8_t slice_number;
 
+/* Function Prototypes */
+void interruptCallback(uint _gpio,  uint32_t _eventMask);
+void setup(void);
+void executeButtonPress(void);
+void TurnOnAirFreshener(void);
+void returnToSteadyState(void);
+void TurnDeviceOff(void);
+/* End Function Prototypes */
 
 void interruptCallback(uint _gpio,  uint32_t _eventMask) {
   if(_gpio == MOTION_SENSOR_PIN) {
@@ -22,13 +34,60 @@ void setup() {
   irq_set_enabled(IO_IRQ_BANK0, true);
 
   /* setup servo that will toggle buttons of device */
-
+  gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM);
+  slice_number = pwm_gpio_to_slice_num(SERVO_PIN);
+  pwm_set_wrap(slice_number, 255);
+  pwm_set_chan_level(slice_number, PWM_CHAN_A, STEADY_STATE_PWM);
+  pwm_set_enabled(slice_number, true);
 }
 
+/*******************************************************************************
+ * Function: executeButtonPress
+ *
+ * Description: Function to handle the pwm's of getting the servo to press the
+ * button on the air freshner device. (added sleeps to allow for time for servo
+ * to move - may need to calibrate the timing for this to work)
+ ******************************************************************************/
 void executeButtonPress() {
-  // Do something
+  pwm_set_chan_level(slice_number, PWM_CHAN_A, BUTTON_PRESS_PWM);
+  sleep_ms(1000);
+}
+
+
+/*******************************************************************************
+ * Function: TurnOnAirFreshener
+ *
+ * Description: Function to execute enough button presses to turn the device on.
+ * (This call will turn my particular device on to the persistent mode)
+ ******************************************************************************/
+void TurnOnAirFreshener() {
+  executeButtonPress();
+  returnToSteadyState();
   MotionDetected = false;
 }
+
+/*******************************************************************************
+ * Function: ReturnToSteadyState
+ *
+ * Description: Return the servo to a state where it is certain to not press a 
+ * button on the device. 
+ ******************************************************************************/
+void returnToSteadyState() {
+  pwm_set_chan_level(slice_number, PWM_CHAN_A, STEADY_STATE_PWM);
+  sleep_ms(1000);
+}
+
+/*******************************************************************************
+ * Function: TurnDeviceOff
+ *
+ * Description: Function to execute enough button presses to turn the device off
+ * after having been turned on b motion detection.
+ ******************************************************************************/
+void TurnDeviceOff() {
+  executeButtonPress();
+  returnToSteadyState();
+}
+
 
 int main() {
   setup();
@@ -40,6 +99,3 @@ int main() {
   }
   return 0;
 }
-
-
-
